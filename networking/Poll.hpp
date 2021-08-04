b5fe45cc81830cb805610ef4f0bd85f269a77020
Ovoda <cdetourrispro@gmail.com>
2021-08-04 18:51:44 +0200

    it's working

---
+++ networking/Poll.hpp
@@ -0,0 +1,46 @@
+#ifndef NETWORK_POLL_HPP
+#define NETWORK_POLL_HPP
+
+#include <poll.h>
+#include "Socket.hpp"
+#include <vector>
+#include <errno.h>
+#include <sys/socket.h>
+#include <unistd.h> // close()
+
+namespace network
+{
+	class Poll
+	{
+	public:
+		Poll(void);
+		Poll(int timeout, int size);
+		Poll(int timeout, std::vector<network::Socket> s);
+
+		~Poll();
+
+		int get_size(void) const;
+		int get_nb_socket(void) const;
+		struct pollfd *get_fds(void) const;
+		int get_timeout(void) const;
+
+		void	add(int fd, int flags);
+		int		do_poll(void);
+		void	check_sockets(std::vector<network::Socket> s);
+		void	check_requests(void);
+		void	resize_fds(void);
+		void	run_servers(std::vector<network::Socket> s);
+
+	private:
+		struct pollfd *_fds;
+		int _timeout;
+		int _capacity;
+		int _size;
+		int _nb_socket;
+		int _ready;
+	};
+}
+
+std::ostream &operator<<(std::ostream &o, network::Poll const &p);
+
+#endif
--- test.cpp
+++ test.cpp
@@ -3,83 +3,26 @@
 #include <stdio.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <vector>
 
-#include "src/network/SimpleRequest.hpp"
-#include "src/network/Socket.hpp"
+#include "networking/Socket.hpp"
+#include "networking/Poll.hpp"
 
-int main(void) {
-    network::Socket s(18000);
+int main(void)
+{
+    network::Socket s1(18000);
+    network::Socket s2(18001);
+    network::Socket s3(18002);
+    std::vector<network::Socket> s;
 
-    struct pollfd fds[200];
-    int timeout = 5000;
-    int ret, current_size, nb_fd;
+    s.push_back(s1);
+    s.push_back(s2);
+    s.push_back(s3);
 
-    fds[0].fd = s.get_id();
-    fds[0].events = POLLIN;
-    current_size = nb_fd = 1;
+    network::Poll p(10000, s);
+    std::cout << p << std::endl;
 
-    for (;;) {
-        ret = poll(fds, current_size, timeout);
-        if (ret < 0) {
-            std::cerr << "Error: Poll() failed" << std::endl;
-            break;
-        }
-        if (ret == 0) {
-            std::cerr << "Timeout" << std::endl;
-            break;
-        }
-        // std::cout << "POLLOUT" << std::endl;
-
-        current_size = nb_fd;
-        for (int i = 0; i < current_size; i++) {
-            if (fds[i].fd == s.get_id()) {
-                // std::cout << "server" << std::endl;
-                if (fds[i].revents != POLLIN) {
-                    // std::cout << "Error: not pollin" << std::endl;
-                    // break;
-                } else {
-                    network::SimpleRequest tmp(s.do_accept());
-                    fcntl(tmp.get_fd(), F_SETFL, O_NONBLOCK);
-                    fds[nb_fd].fd = tmp.get_fd();
-                    fds[nb_fd].events = POLLOUT | POLLIN;
-                    nb_fd++;
-                    std::cout << "accept" << std::endl;
-                }
-            } else {
-                // std::cout << "request" << std::endl;
-                if (fds[i].revents & POLLIN) {
-                    char buffer[3000];
-                    ret = recv(fds[i].fd, buffer, 3000, 0);
-                    if (ret < 0) {
-                        break;
-                    }
-                    std::cout << "HELLO: " << buffer << std::endl;
-                }
-                //     std::cout << "sending" << std::endl;
-                //     send(fds[i].fd, "bonjour", 7, 0);
-                //     int errorCode;
-                //     uint len = sizeof(errorCode);
-                //     int result = getsockopt(fds[i].fd, SOL_SOCKET, SO_ERROR,
-                //                             &errorCode, &len);
-                //     if (errorCode == 0) {
-                //         close(fds[i].fd);
-                //         fds[i].fd = -1;
-                //         for (int k = 0; k < nb_fd; k++) {
-                //             if (fds[k].fd == -1) {
-                //                 for (int j = k; j < nb_fd; j++) {
-                //                     fds[j].fd = fds[j + 1].fd;
-                //                 }
-                //                 k--;
-                //                 nb_fd--;
-                //             }
-                //         }
-                //     }
-                //     // fds[0].events = POLLIN;
-                // }
-            }
-        }
-        current_size = nb_fd;
-    }
+    p.run_servers(s);
 
     return (0);
 }
